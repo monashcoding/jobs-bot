5bab9fa4daa8e8669d815f87f1259134ec251106
Jason Yi <jasonyi1413@gmail.com>
2026-07-26 15:09:12 +1000

    feat: remove unecessary intents

--- src/bot.py
+++ src/bot.py
@@ -16,8 +16,6 @@ _log = logging.getLogger(__name__)
 load_dotenv()
 
 intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
